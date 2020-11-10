306823be7e329cd72fa26064bb7cff1051f7ea7a
kmykoh <kmykoh97@gmail.com>
2020-11-10 21:01:41 +0800

    modified kernel information

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Optimus Drunk Kernel by GtrCraft
+kernel.string=My Kernel by MyKoh
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
